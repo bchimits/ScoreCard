7bb0384c439c22c9527046ff88d02fe3437bf8ab
Brandon Chimits <bjchimits@gmail.com>
2026-06-14 14:25:29 -0700

    Initial Commit

---
+++ ScoreCard/ScoreCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreCardApp.swift
+//  ScoreCard
+//
+//  Created by Brandon Chimits on 6/14/26.
+//
+
+import SwiftUI
+
+@main
+struct ScoreCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
